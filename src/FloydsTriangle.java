003f44534b7d7519840d9a7a2927402289d4918a
Nitish Kumar Mangalam <mangalamnitish@gmail.com>
2025-06-18 20:34:01 +0530

    Updated Floyd's Triangle pattern with correct logic

--- src/FloydsTriangle.java
+++ src/FloydsTriangle.java
@@ -1,2 +1,15 @@
 public class FloydsTriangle {
+    public static void main(String[] args) {
+        floydPattern(7);
+    }
+    static void floydPattern(int n) {
+        int number = 1;
+        for (int row = 1; row <=n ; row++) {
+            for (int col = 1; col <=row ; col++) {
+                System.out.print(number+" ");
+                number+=2;
+            }
+            System.out.println();
+        }
+    }
 }
