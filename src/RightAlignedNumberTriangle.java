e713d70a2e92a9ece0e3b3cce6da6fb0e1dbae0b
Nitish Kumar Mangalam <mangalamnitish@gmail.com>
2025-06-19 20:16:39 +0530

    Updated RightAlignedNumberTriangle with full logic

--- src/RightAlignedNumberTriangle.java
+++ src/RightAlignedNumberTriangle.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class RightAlignedNumberTriangle {
+public class RightAlignedNumberTriangle {
+    public static void main(String[] args) {
+patternTriangle(7);
+    }
+    static void patternTriangle(int n) {
+        for (int row = 1; row <= n; row++) {
+            // for spaces
+            for (int space = 1; space <=n-row ; space++) {
+                System.out.print("  ");
+            }
+            for (int col = 1; col <=row ; col++) {
+                System.out.print(col +" ");
+            }
+            System.out.println();
+        }
+    }
+    
 }
